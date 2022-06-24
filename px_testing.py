2c1436b4c8e753030f42e0b98e1bfb2c3b78cee9
Alejandro Ascárate <106303166+aleazk@users.noreply.github.com>
2022-06-24 11:54:59 -0300

    Update px_testing.py

--- px_testing.py
+++ px_testing.py
@@ -2,12 +2,8 @@ import pienex as px
 
 r = px.reasoning(((1, 1, 2), 3, -3))
 
-#r.programme()
+r.programme()
 
-print (r.ndpredict("ND")[70:80])
+ndpredictions = r.ndpredict("ND")
 
-import numpy as np
-
-ND_distance = np.loadtxt("ND_distance.csv", delimiter=",")
-
-print (ND_distance[70:80])
+print (ndpredictions)
